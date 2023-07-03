74820ddf30e4e03b562f212e9239a439dfc8bec5
Оксана Афанасьєва <afoksana31@gmail.com>
2023-07-03 07:51:12 +0200

    .

--- Forms/InformationRoute.cs
+++ Forms/InformationRoute.cs
@@ -32,29 +32,34 @@ namespace BusStationCashDesk.Windows_Forms
             {
                 if (routeList[i].Number == selected && selected != null)
                 {
-                    TextInfo title = CultureInfo.InvariantCulture.TextInfo;
-
-                    numberRouteTextBox.Text = routeList[i].Number;
-                    fromTextBox.Text = title.ToTitleCase(routeList[i].FromName ?? string.Empty);
-                    toTextBox.Text = title.ToTitleCase(routeList[i].ToName ?? string.Empty);
-                    fromDateTimePicker.Value = routeList[i].DateTimeFrom;
-                    timeFromTextBox.Text = routeList[i].TimeFrom;
-                    toDateTimePicker.Value = routeList[i].DateTimeTo;
-                    timeToTextBox.Text = routeList[i].TimeTo;
-                    listStop.View = View.Details;
-
-                    for (int j = 0; j < routeList[i].Stops.Count; j++)
-                    {
-                        ListViewItem item = new ListViewItem(title.ToTitleCase(routeList[i].Stops[j] 
-                            ?? string.Empty));
-                        item.SubItems.Add(routeList[i].TimeStop[j]);
-                        listStop.Items.Add(item);
-                    }
-
-                    freePlaceTextBox.Text = (routeList[i].FreeSeats).ToString();
-                    priceTextBox.Text = routeList[i].Price;
+                    DisplayRoute(i);
                 }
             }
+        }
+
+        private void DisplayRoute(int i)
+        {
+            TextInfo title = CultureInfo.InvariantCulture.TextInfo;
+
+            numberRouteTextBox.Text = routeList[i].Number;
+            fromTextBox.Text = title.ToTitleCase(routeList[i].FromName ?? string.Empty);
+            toTextBox.Text = title.ToTitleCase(routeList[i].ToName ?? string.Empty);
+            fromDateTimePicker.Value = routeList[i].DateTimeFrom;
+            timeFromTextBox.Text = routeList[i].TimeFrom;
+            toDateTimePicker.Value = routeList[i].DateTimeTo;
+            timeToTextBox.Text = routeList[i].TimeTo;
+            listStop.View = View.Details;
+
+            for (int j = 0; j < routeList[i].Stops.Count; j++)
+            {
+                ListViewItem item = new ListViewItem(title.ToTitleCase(routeList[i].Stops[j]
+                    ?? string.Empty));
+                item.SubItems.Add(routeList[i].TimeStop[j]);
+                listStop.Items.Add(item);
+            }
+
+            freePlaceTextBox.Text = (routeList[i].FreeSeats).ToString();
+            priceTextBox.Text = routeList[i].Price;
         }
     }
 }
